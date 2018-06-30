74a745bb62208dd6ada12076be6f44db824f0238
Kaiqgs <kags_@live.com>
2018-06-29 21:51:50 -0300

    Finite State Automata.
    A windows forms .net, implementation of a Finite Automata;

---
+++ FiniteAutomata/FiniteAutomata/Automata/StringOperacoes.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FiniteAutomata.Automata;
+
+
+namespace FiniteAutomata.Automata
+{
+    using Sigma = List<char>;
+    using q = Int32;
+
+    static class OperacoesAutomata
+    {
+        //Alfabeto padrão definido com caracteres básicos;
+        public static Sigma StdSigma = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz _".ToList();
+        
+        //Definição de um gerador de automato reconhecedor de prefixo, dado prefixo e alfabeto;
+        public static Automata ComecaCom(string prefixo, Sigma sigma)
+        {
+            if (!EhValido(prefixo, sigma)) return null;
+            var auto = new Automata();
+            q Q0 = auto.CriaQ0(0);
+
+            int i;
+            for (i = 0; i < prefixo.Length; ++i)
+            {
+                q Qa = auto.CriaQ(i);
+                q Qb = auto.CriaQ(i + 1);
+
+                auto.Delta.Add(new Transicao(Qa, prefixo[i], Qb));
+            }
+            q Qn = auto.CriaQf(i);
+            sigma.ForEach(simb => auto.Delta.Add(new Transicao(Qn, simb, Qn)));
+            return auto;
+        }
+
+        //Definição de um gerador de automato reconhecedor de suffixo, dado suffixo e alfabeto;
+        public static Automata TerminaCom(string suffixo, Sigma sigma)
+        {
+            if (!EhValido(suffixo, sigma)) return null;
+
+            var auto = new Automata();
+            q Q0 = auto.CriaQ0(0);
+            q Q1 = auto.CriaQ(1);
+            int i = 0;
+            var primeiraLetra = suffixo[0];
+
+            auto.Delta.Add(new Transicao(Q1, suffixo[0], Q1));
+
+            for (i = 0; i < suffixo.Length; ++i)
+            {
+                q Qa = auto.CriaQ(i);
+                q Qb = auto.CriaQ(i + 1);
+                auto.Delta.Add(new Transicao(Qa, suffixo[i], Qb));
+                sigma.ForEach(sim =>
+                {
+                    if (sim != suffixo[i]) auto.Delta.Add(new Transicao(Qa, sim, Q0));
+                    else if (sim == primeiraLetra) auto.Delta.Add(new Transicao(Qa, sim, Q1));
+                });
+            }
+
+            q Qn = auto.CriaQf(i);
+            foreach (var sim in sigma)
+            {
+                if (sim == primeiraLetra) auto.Delta.Add(new Transicao(Qn, sim, Q1));
+                else auto.Delta.Add(new Transicao(Qn, sim, Q0));
+            }
+            return auto;
+        }
+
+        //Definição de um gerador de automato reconhecedor de sub-palavra, dado sub-palavra e alfabeto;
+        public static Automata Contem(string subPalavra, Sigma sigma)
+        {
+            if (!EhValido(subPalavra, sigma)) return null;
+            Automata auto = new Automata();
+            int i;
+            q Q0 = auto.CriaQ0(0);
+
+            for (i = 0; i < subPalavra.Length; ++i)
+            {
+                q Qa = auto.CriaQ(i);
+                q Qb = auto.CriaQ(i + 1);
+
+                auto.Delta.Add(new Transicao(Qa, subPalavra[i], Qb));
+                foreach (var simbolo in sigma)
+                {
+                    if (simbolo != subPalavra[i])
+                    {
+                        auto.Delta.Add(new Transicao(Qa, simbolo, Q0));
+                    }
+                }
+            }
+            q Qn = auto.CriaQf(i);
+            sigma.ForEach(simb => auto.Delta.Add(new Transicao(Qn, simb, Qn)));
+            return auto;
+        }
+
+        //Caso palavra w, não esteja contido em sigma, é inválido;
+        private static bool EhValido(string w, Sigma sigma)
+        {
+            foreach (var letra in w) if (!sigma.Contains(letra)) return false;
+            return true;
+        }
+    }
+}
---
+++ FiniteAutomata/FiniteAutomata/Main.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace FiniteAutomata
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.TesteTextBox = new System.Windows.Forms.TextBox();
+            this.ContemCheckBox = new System.Windows.Forms.CheckBox();
+            this.ComecaCheckBox = new System.Windows.Forms.CheckBox();
+            this.TerminaCheckBox = new System.Windows.Forms.CheckBox();
+            this.InfoButton = new System.Windows.Forms.Button();
+            this.InputLabel = new System.Windows.Forms.Label();
+            this.TestLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(46, 89);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(154, 20);
+            this.InputTextBox.TabIndex = 0;
+            this.InputTextBox.TextChanged += new System.EventHandler(this.Atualiza);
+            // 
+            // TesteTextBox
+            // 
+            this.TesteTextBox.Location = new System.Drawing.Point(46, 132);
+            this.TesteTextBox.Name = "TesteTextBox";
+            this.TesteTextBox.Size = new System.Drawing.Size(154, 20);
+            this.TesteTextBox.TabIndex = 1;
+            this.TesteTextBox.TextChanged += new System.EventHandler(this.Reconhece);
+            // 
+            // ContemCheckBox
+            // 
+            this.ContemCheckBox.AutoCheck = false;
+            this.ContemCheckBox.AutoSize = true;
+            this.ContemCheckBox.Location = new System.Drawing.Point(12, 35);
+            this.ContemCheckBox.Name = "ContemCheckBox";
+            this.ContemCheckBox.Size = new System.Drawing.Size(65, 17);
+            this.ContemCheckBox.TabIndex = 1;
+            this.ContemCheckBox.TabStop = false;
+            this.ContemCheckBox.Text = "Contém;";
+            this.ContemCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // ComecaCheckBox
+            // 
+            this.ComecaCheckBox.AutoCheck = false;
+            this.ComecaCheckBox.AutoSize = true;
+            this.ComecaCheckBox.Location = new System.Drawing.Point(12, 12);
+            this.ComecaCheckBox.Name = "ComecaCheckBox";
+            this.ComecaCheckBox.Size = new System.Drawing.Size(92, 17);
+            this.ComecaCheckBox.TabIndex = 1;
+            this.ComecaCheckBox.TabStop = false;
+            this.ComecaCheckBox.Text = "Começa Com;";
+            this.ComecaCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // TerminaCheckBox
+            // 
+            this.TerminaCheckBox.AutoCheck = false;
+            this.TerminaCheckBox.AutoSize = true;
+            this.TerminaCheckBox.Location = new System.Drawing.Point(12, 58);
+            this.TerminaCheckBox.Name = "TerminaCheckBox";
+            this.TerminaCheckBox.Size = new System.Drawing.Size(91, 17);
+            this.TerminaCheckBox.TabIndex = 10;
+            this.TerminaCheckBox.TabStop = false;
+            this.TerminaCheckBox.Text = "Termina Com;";
+            this.TerminaCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // InfoButton
+            // 
+            this.InfoButton.Location = new System.Drawing.Point(160, 12);
+            this.InfoButton.Name = "InfoButton";
+            this.InfoButton.Size = new System.Drawing.Size(40, 23);
+            this.InfoButton.TabIndex = 11;
+            this.InfoButton.Text = "info";
+            this.InfoButton.UseVisualStyleBackColor = true;
+            this.InfoButton.Click += new System.EventHandler(this.InfoButton_Click);
+            // 
+            // InputLabel
+            // 
+            this.InputLabel.AutoSize = true;
+            this.InputLabel.Location = new System.Drawing.Point(9, 92);
+            this.InputLabel.Name = "InputLabel";
+            this.InputLabel.Size = new System.Drawing.Size(31, 13);
+            this.InputLabel.TabIndex = 12;
+            this.InputLabel.Text = "Input";
+            // 
+            // TestLabel
+            // 
+            this.TestLabel.AutoSize = true;
+            this.TestLabel.Location = new System.Drawing.Point(9, 132);
+            this.TestLabel.Name = "TestLabel";
+            this.TestLabel.Size = new System.Drawing.Size(34, 13);
+            this.TestLabel.TabIndex = 12;
+            this.TestLabel.Text = "Teste";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(212, 161);
+            this.Controls.Add(this.TestLabel);
+            this.Controls.Add(this.InputLabel);
+            this.Controls.Add(this.InfoButton);
+            this.Controls.Add(this.TerminaCheckBox);
+            this.Controls.Add(this.ComecaCheckBox);
+            this.Controls.Add(this.ContemCheckBox);
+            this.Controls.Add(this.TesteTextBox);
+            this.Controls.Add(this.InputTextBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Main";
+            this.Text = "Automato Finito";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.TextBox TesteTextBox;
+        private System.Windows.Forms.CheckBox ContemCheckBox;
+        private System.Windows.Forms.CheckBox ComecaCheckBox;
+        private System.Windows.Forms.CheckBox TerminaCheckBox;
+        private System.Windows.Forms.Button InfoButton;
+        private System.Windows.Forms.Label InputLabel;
+        private System.Windows.Forms.Label TestLabel;
+    }
+}
+
